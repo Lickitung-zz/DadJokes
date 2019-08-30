4ab8c97bc0fa6ea8207444cf69de936355b1e171
SIeep <brandon_n_evans@live.com>
2019-08-30 11:20:42 -0500

    add some styling, update default.vue layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,30 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        dadjokes
-      </h1>
-      <h2 class="subtitle">
-        some dad jokes, using nuxt.js
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    <h2>Welcome to best corny dad jokes app on the web</h2>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+
 
 export default {
-  components: {
-    Logo
+  head() {
+      return {
+          title: 'Welcome To Dad Jokes',
+          meta: [
+              {
+                  hid: 'description',
+                  name: 'description',
+                  content: 'Best place for corny dad jokes'
+              }
+          ]
+      }
   }
 }
 </script>
 
 <style>
-.container {
+/* .container {
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
@@ -68,5 +53,5 @@ export default {
 
 .links {
   padding-top: 15px;
-}
+} */
 </style>
