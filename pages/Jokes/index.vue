8eec0ed7369a076e2094e5c87edc88a7f6280aa4
SIeep <brandon_n_evans@live.com>
2019-08-30 12:04:26 -0500

    add search

--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <SearchJokes v-on:search-text="searchText"/>
         <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke"/>
     </div>
 </template>
@@ -7,10 +8,12 @@
 <script>
 import axios from "axios"
 import Joke from '../../components/Joke'
+import SearchJokes from '../../components/SearchJokes'
 
     export default {
         components: {
-            Joke
+            Joke,
+            SearchJokes
         },
         data() {
             return {
@@ -30,6 +33,21 @@ import Joke from '../../components/Joke'
                 console.log(error)
             }
            
+        },
+        methods: {
+            async searchText(text) {
+                const config = {
+                    headers: {
+                        'Accept': 'application/json'
+                    }
+                }
+                try {
+                    const res = await axios.get(`https://icanhazdadjoke.com/search?term=${text}`, config);
+                    this.jokes = res.data.results;
+                } catch (error) {
+                    console.log(error)
+                }
+            }
         },
         head() {
             return {
